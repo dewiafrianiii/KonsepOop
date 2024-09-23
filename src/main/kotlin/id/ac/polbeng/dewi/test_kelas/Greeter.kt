91e65502a97079fc1b26b1c98fe6d00693cca713
dewiafrianiii <afrianidewi551@gmail.com>
2024-09-23 17:09:27 +0700

    push stater project.

--- src/main/kotlin/id/ac/polbeng/dewi/test_kelas/Greeter.kt
+++ src/main/kotlin/id/ac/polbeng/dewi/test_kelas/Greeter.kt
@@ -1,3 +1,5 @@
+package id.ac.polbeng.dewi.test_kelas
+
 class Greeter {
     fun greet() {
         println("Hello object world!")
--- src/main/kotlin/id/ac/polbeng/dewi/test_kelas/GreeterTest.kt
+++ src/main/kotlin/id/ac/polbeng/dewi/test_kelas/GreeterTest.kt
@@ -1,3 +1,5 @@
+package id.ac.polbeng.dewi.test_kelas
+
 fun main() {
     val greeter = Greeter()
     greeter.greet()
